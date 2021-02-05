25c1546da3dd24219471f2864163606b56427866
SeonHwan <shkim7902@anipen.com>
2021-02-05 14:28:42 +0900

    주석 추가

--- Assets/InstancedProcedural.cs
+++ Assets/InstancedProcedural.cs
@@ -28,6 +28,13 @@ public class InstancedProcedural : MonoBehaviour
         computeShader.SetFloat(TimeID, Time.time);
         computeShader.SetBuffer(0, PositionID, positionBuffer);
         
+        //resolution : 128
+        //16,384 개의 스레드가 돌아야 함
+        //커널함수 하나가 8 * 8 = 64 개의 스레드를 갖고 있으니
+        //16,384 / 64 = 256 개의 그룹을 돌려야 함
+        //루트256 = 16 이므로 각 그룹은 16이어야 함
+        //16은 즉 resolution / 8 과 같으므로
+        //groups = resoltion / 8 임
         int groups = Mathf.CeilToInt(count / 8f);
         computeShader.Dispatch(0, groups, groups, 1);
         
